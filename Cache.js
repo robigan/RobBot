3e868d256d37c342c1174daf04e065b718c57804
robigan <35210888+robigan@users.noreply.github.com>
2021-04-28 16:07:24 +0200

    Added, mongoDB support and added Cache to index

--- Cache.js
+++ Cache.js
@@ -10,12 +10,11 @@ const CacheClientConfig = require("./Configs/CacheClient.json");
 /**
  * Wrapper function for initiating CacheClient
  * @param {object} Config
- * @param {import("./amqp/AmqpClient")} AmqpClient
  * @returns {import("./Cache/CacheClient")} CacheClient
  */
-module.exports = async (Config = Object.assign(CacheClientConfig, MainConfig, SecretConfig), AmqpClient = new (require("./amqp/AmqpClient.js"))(MainConfig.amqp)/*, LocResProvided = require("./LocationResolver.js")*/) => {
+module.exports = async (Config = Object.assign(CacheClientConfig, MainConfig, SecretConfig)/*, AmqpClient = new (require("./amqp/AmqpClient.js"))(MainConfig.amqp)/*, LocResProvided = require("./LocationResolver.js")*/) => {
     //global.LocRes = LocResProvided;
-    const Cache = new CacheClient(Config, AmqpClient);
-    //await Cache.start();
+    const Cache = new CacheClient(Config/*, AmqpClient*/);
+    await Cache.start();
     return Cache;
 };
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // JavaScript source code main entry point
 //const Code = require("./Code.js");
 const Gateway = require("./Gateway.js");
+const Cache = require("./Cache.js");
 
 const AmqpClient = new (require("./amqp/AmqpClient.js"))(require("./Configs/Config.json").amqp);
 
 (async () => {
     await AmqpClient.start();
-    //Start(Code);
+    Cache();
     Gateway(undefined, AmqpClient);
 })().catch(console.error);
